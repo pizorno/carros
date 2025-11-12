120dbf8eb37fc5b4cee7843712e39c696b4999cb
Eduardo Pizorno <pizorno@gmail.com>
2025-11-11 22:38:58 -0300

    Adding rename images on admin upload

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+# Source - https://stackoverflow.com/a
+# Posted by M-Jamiri
+# Retrieved 2025-11-11, License - CC BY-SA 4.0
+import os
+from uuid import uuid4
+
+def path_and_rename(instance, filename):
+    upload_to = 'cars/'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -15,7 +34,7 @@ class Car(models.Model):
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    photo = models.ImageField(upload_to=path_and_rename, blank=True, null=True)
 
     def __str__(self):
         return self.model
