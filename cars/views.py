f18c82874f8e5f216c9d6dd45048c0c01ccb2b56
Eduardo Pizorno <pizorno@gmail.com>
2025-11-12 11:52:00 -0300

    Using django orm to get data

--- cars/views.py
+++ cars/views.py
@@ -3,6 +3,7 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.all()
+    # cars = Car.objects.all()
+    cars = Car.objects.filter(brand__name='Chevrolet')
     return render(request, "cars.html", {"cars": cars})
 
