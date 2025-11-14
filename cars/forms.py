ccf404c6f7092433c6e077431e1674ee1b7a9fd9
Eduardo Pizorno <pizorno@gmail.com>
2025-11-14 18:15:36 -0300

    Validate fields on form

--- cars/forms.py
+++ cars/forms.py
@@ -37,3 +37,15 @@ class CarModelForm(forms.ModelForm):
             'value': 'Valor',
             'photo': 'Imagem',
         }
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value is not None and value <= 0:
+            self.add_error('value', 'O valor deve ser maior que zero.')
+        return value
+    
+    def clean_factory_year(self):   
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year <= 1974:
+            self.add_error('factory_year', 'O ano de fabricação não pode ser menor que 1974.')
+        return factory_year
